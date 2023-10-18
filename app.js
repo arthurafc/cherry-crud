033a83516a21b53140869f6a9325cbcaea93b8c3
Arthur Ferreira da Cruz <arthur.afc3@gmail.com>
2023-10-18 18:19:32 -0300

    inserted first comms between files

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const config = require('./config.js');
+const PORT = config.PORT;
+
+const functions = require("functions.js");
+
+app.get("/status", (request, response) => {
+  const status = {
+    "Status": "Running"
+  };
+  response.send(status);
+});
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
+});
+
+/*
+app.get('/groups/:n', (request, response) => {
+  const n = request.params.n;
+  const answer = `${n}! = ${calcFactorial(n)}`
+  
+  response.send(answer);
+});
+*/
---
+++ functions.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const rawData = fs.readFileSync('/db/groups.json');
+const groups = JSON.parse(rawData);
+
+const rawData2 = fs.readFileSync('/db/members.json');
+const members = JSON.parse(rawData);
+
+//module.exports = { functionName1, funtionName2, functionNameN };
